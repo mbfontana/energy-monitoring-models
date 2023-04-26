71b7ad8bae8be4b4cab6c1d0076033408971b2d1
Matheus Fontana <mbfontana@outlook.com>
2023-04-26 20:39:05 -0300

    MInor changes to web server | New sample dataset

--- server.py
+++ server.py
@@ -34,68 +34,71 @@ previous_sample = pd.DataFrame([{
 # Global variable to store the current connected appliances
 connected_appliances = []
 # Global variable to identify if a appliance was connected or disconnected
-connected_new_appliance = True
+load_change = None
 
 # Define a function to map the index with its appliance name
+
+
 def map_appliance(index):
     match(index):
         case 1:
-            return "Appliance 1"
+            return "Hairdryer"
         case 2:
-            return "Appliance 2"
+            return "Iron"
         case 3:
-            return "Appliance 3"
+            return "Blender"
         case 4:
-            return "Appliance 4"
+            return "Toaster"
         case 5:
-            return "Appliance 5"
+            return "Notebook"
         case 6:
-            return "Appliance 6"
+            return "Sandwich maker"
         case 7:
-            return "Appliance 7"
-        case 8:
-            return "Appliance 8"
+            return "Airfryer"
 
 # Define a function to preprocess the input data
+
+
 def preprocess_data(data):
     global previous_sample
-    global connected_new_appliance
+    global load_change
 
     # Convert the input data to a pandas dataframe
-    current_sample = pd.DataFrame([data], columns=["fft01", "fft03", "fft05", "fft07", "fft09", "fft11",
-                                                   "fft13", "fft15", "fft17", "fft19", "fft21", "fft23", "fft25", "fft27", "fft29", "fft31"])
-    # Get mean values
-    current_sample_mean = current_sample.sum().sum() / 16
-    previous_sample_mean = previous_sample.sum().sum() / 16
-    print(current_sample_mean)
-    print(previous_sample_mean)
+    json = pd.DataFrame([data], columns=["comparison", "fft01", "fft03", "fft05", "fft07", "fft09", "fft11",
+                                         "fft13", "fft15", "fft17", "fft19", "fft21", "fft23", "fft25", "fft27", "fft29", "fft31"])
+    current_sample = json.iloc[:, 1:]
+    load_change = json.iloc[:, 0:1]
 
-    if current_sample_mean > previous_sample_mean:
+    if load_change["comparison"][0] == "Connected":
         # Appliance connected
-        connected_new_appliance = True
         df = current_sample - previous_sample
         previous_sample = current_sample
         return df
-    elif current_sample_mean < previous_sample_mean:
+    elif load_change["comparison"][0] == "Disconnected":
         # Appliance disconnected
-        connected_new_appliance = False
         df = previous_sample - current_sample
         previous_sample = current_sample
         return df
 
 # Define a function to postprocess the predicted labels
+
+
 def postprocess_data(prediction):
     labels_str = str(prediction)
     labels_lst = [x == 'True' for x in labels_str.strip('[]').split()]
-    appliance = [map_appliance(
-        i+1) for i in range(len(labels_lst)) if labels_lst[i] == True]
-    return appliance[0]
+    appliance = [i+1 for i in range(len(labels_lst)) if labels_lst[i] == True]
+    if appliance != []:
+        return appliance[0]
+    else:
+        return None
 
 # Define a route for making predictions with the trained model
+
+
 @app.route("/predict", methods=["POST"])
 def predict():
     global connected_appliances
-    global connected_new_appliance
+    global load_change
 
     # Get the input data from the request body
     input_data = request.get_json()
@@ -106,13 +109,22 @@ def predict():
     # Use the trained model to make predictions on the preprocessed data
     prediction = rf_model.predict(preprocessed_data)
 
-    appliance = postprocess_data(prediction)
+    # Predicted appliance
+    appliance_id = postprocess_data(prediction)
+    appliance = map_appliance(appliance_id)
 
-    if connected_new_appliance:
+    if load_change["comparison"][0] == "Connected" and appliance not in connected_appliances:
         connected_appliances.append(appliance)
-    else:
+    elif load_change["comparison"][0] == "Disconnected" and appliance in connected_appliances:
         connected_appliances.remove(appliance)
 
+    print(connected_appliances)
+    # Return the list with all connected appliances
+    return jsonify(appliance_id)
+
+
+@app.route("/share_predictions", methods=["GET"])
+def share_predictions():
     # Return the list with all connected appliances
     return jsonify(connected_appliances)
 
